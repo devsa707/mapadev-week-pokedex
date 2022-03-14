159c81019fc2057b3476a5cea2ac868c09259e16
devsa707 <101226180+devsa707@users.noreply.github.com>
2022-03-14 19:58:18 -0300

    Correção da listagem lateral
    Listagem lateral não estava com o get da cor

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,16 @@ listaSelecaoPokemons.forEach(pokemon => {
         const cartaoPokemonAberto = document.querySelector('.aberto')
         cartaoPokemonAberto.classList.remove('aberto')
 
+        const listagemPokemonAtivo = document.querySelector('.ativo')
+        listagemPokemonAtivo.classList.remove('ativo')
+
         const idPokemonSelecionado = pokemon.attributes.id.value
 
         const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
         cartaoPokemonParaAbrir.classList.add('aberto')
-console.log(idPokemonSelecionado)
+
+        const listagemPokemonParaAtivar = document.getElementById(idPokemonSelecionado)
+        listagemPokemonParaAtivar.classList.add('ativo')        
 
     })
 })
